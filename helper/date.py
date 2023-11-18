bb0331a39283421fa8e77f62395684a79b66e726
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-11-18 12:00:05 +0530

    Update date.py

--- helper/date.py
+++ helper/date.py
@@ -1,21 +1,36 @@
-from datetime import timedelta, date ,datetime
+from datetime import timedelta, date, datetime
 import time
 
-def add_date():
-	today = date.today()
-	ex_date = today + timedelta(days=30)
-	pattern = '%Y-%m-%d'
-	epcho = int(time.mktime(time.strptime(str(ex_date), pattern)))
-	normal_date = datetime.fromtimestamp(epcho).strftime('%Y-%m-%d')
-	return epcho , normal_date
+def calculate_expiration_date():
+    """
+    Calculates the expiration date by adding 30 days to the current date.
 
-def check_expi(saved_date):
-	today = date.today()
-	pattern = '%Y-%m-%d'
-	epcho = int(time.mktime(time.strptime(str(today), pattern)))
-	then = saved_date - epcho
-	print(then)
-	if then > 0:
-		return True
-	else:
-		return False
+    Returns:
+        tuple: (epoch timestamp, formatted normal date)
+    """
+    today = date.today()
+    expiration_date = today + timedelta(days=30)
+    pattern = '%Y-%m-%d'
+    epoch = int(time.mktime(time.strptime(str(expiration_date), pattern)))
+    normal_date = datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
+    return epoch, normal_date
+
+def is_date_valid(saved_date):
+    """
+    Checks if a given saved date is still valid.
+
+    Args:
+        saved_date (int): The saved date as an epoch timestamp.
+
+    Returns:
+        bool: True if the date is still valid, False otherwise.
+    """
+    today = date.today()
+    pattern = '%Y-%m-%d'
+    current_epoch = int(time.mktime(time.strptime(str(today), pattern)))
+    time_difference = saved_date - current_epoch
+
+    if time_difference > 0:
+        return True
+    else:
+        return False
